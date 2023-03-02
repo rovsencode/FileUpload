b16e52bcca171623968318b3b92adac59b991d26
rovsencode <rovshanakh@code.edu.az>
2023-03-02 18:19:33 +0400

    add create

---
+++ FirelloProject/Areas/AdminArea/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using FirelloProject.DAL;
+using FirelloProject.Models;
+using FirelloProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirelloProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_appDbContext.Categories.ToList());
+
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Category category=_appDbContext.Categories.SingleOrDefault(c=>c.ID==id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryCreateVM category)
+        {
+            if (!ModelState.IsValid) return View();
+
+
+            Category newCategory = new() { 
+            Name= category.Name,
+            Description= category.Description,
+            };
+
+            _appDbContext.Categories.Add(newCategory);
+            _appDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FirelloProject/Services/Basket/BasketProductCount.cs
@@ -0,0 +1,28 @@
+﻿using FirelloProject.ViewModels;
+using Newtonsoft.Json;
+
+namespace FirelloProject.Services.Basket
+{
+    public class BasketProductCount : IBasketProductCount
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public BasketProductCount(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public int CalculateBasketProductCount()
+        { 
+            string basket = _httpContextAccessor.HttpContext.Request.Cookies["basket"];
+          
+            
+                var products = JsonConvert.DeserializeObject<List<BasketVM>>(basket);
+
+
+          return products.Sum(p => p.BasketCount);
+
+
+        }
+    }
+}
